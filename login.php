1f2bffaec8d94bd6de08642a536edef4d2fadca7
Roman <ramzes.murzakov99@gmail.com>
2021-08-07 16:13:33 -0400

    Initial Commit

--- login.php
+++ login.php
@@ -2,17 +2,16 @@
 // Подключаем коннект к БД
 require_once 'db.php';
 
-	echo '<br /> 
+	/*echo '<br /> 
 	<form method="post">
 	ЛОГИН: <input type="text" name="username_input" /> <br /> 
 ПАРОЛЬ: <input type="text" name="password_input" /> <br />
 <input type="submit" name="submit" value="Login" />
-</form>';
+</form>';*/
 
 
 $username_input = $_POST['username_input'];
 $password_input = $_POST['password_input'];
-
 if ($result = mysqli_query($db, "SELECT `username` FROM `users` WHERE `username`='" . $username_input . "' and `password`='" . $password_input . "'")) {
         if( mysqli_num_rows($result) == 1) { 
                 echo "Аккаунт подтверждён";
@@ -21,3 +20,30 @@ if ($result = mysqli_query($db, "SELECT `username` FROM `users` WHERE `username`
             }
         } 
 ?>
+
+<!DOCTYPE html>
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <form method="post" action="" class="login">
+    <p>
+      <label for="login">Логин:</label>
+      <input type="text" name="username_input" id="login" value="Логин">
+    </p>
+
+    <p>
+      <label for="password">Пароль:</label>
+      <input type="password" name="password_input" id="password" value="4815162342">
+    </p>
+
+    <p class="login-submit">
+      <button type="submit" class="login-button">Войти</button>
+    </p>
+  </form>
+</body>
+</html>
